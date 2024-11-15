984b61370bea162869b41552e7b063399ce36eee
GUILHERME AGOSTINHO SILVA <guilherme_silva53@estudante.sesisenai.org.br>
2024-11-14 21:17:57 -0300

    TELAS RENOMEADOS E TELA DE LISTA DE HORAIOS FEITA

---
+++ tela_de_ListaHorarios/src/components/Body/HorasPartida.jsx
@@ -0,0 +1,137 @@
+import React from 'react';
+import './HorasPartida.css';
+import { FiArrowLeft, FiArrowRight} from 'react-icons/fi';
+import Logo from '../Logo/Logo';
+const HorasPartida = () => {
+
+
+  
+
+  return (
+    <div className="container-master">
+      <div className='topo'>
+        <div className='topoLogo'>
+          <Logo/>
+        </div>
+        <div className='topoTitulo'>
+          <label className='topoLabel'>FUT AGENDA</label>
+        </div>
+        
+      </div>
+            <div className='meio'>
+              <div className='meioCima'>
+
+                  <div className='quadrados1'>
+                    <div className='parteCima'>
+                      <div className='espaco1'><img className='icon' src="./imagens/calendario.png"/></div>
+                      <div className='espaco2'><label className=''>30 de fevereiro</label></div>
+                    </div>
+                    <div className='parteMeio'>
+                    <div className='espaco1'><img className='icon' src="./imagens/relogio.png"/></div>
+                      <div className='espaco2'><label className=''>09:00</label></div>
+                    </div>
+                    <div className='parteBaixo'>
+                    <div className='espaco1'><img  className='icon' src="./imagens/gps.png"/></div>
+                    <div className='espaco2'><label className=''>Rod. joão gualberto suares - 999</label></div>
+                    </div>
+                    <div className='Divbotao'>
+                      <button className='botao'>alugar</button>
+                    </div>
+                  </div>
+                  <div className='quadrados2'>
+                    <div className='parteCima'>
+                      <div className='espaco1'><img className='icon' src="./imagens/calendario.png"/></div>
+                      <div className='espaco2'><label className=''>30 de fevereiro</label></div>
+                    </div>
+                    <div className='parteMeio'>
+                    <div className='espaco1'><img className='icon' src="./imagens/relogio.png"/></div>
+                      <div className='espaco2'><label className=''>10:00</label></div>
+                    </div>
+                    <div className='parteBaixo'>
+                    <div className='espaco1'><img  className='icon' src="./imagens/gps.png"/></div>
+                    <div className='espaco2'><label className=''>Rod. joão gualberto suares - 999</label></div>
+                    </div>
+                    <div className='Divbotao'>
+                      <button className='botao'>alugar</button>
+                    </div>
+                  </div>
+                  <div className='quadrados3'>
+                    <div className='parteCima'>
+                      <div className='espaco1'><img className='icon' src="./imagens/calendario.png"/></div>
+                      <div className='espaco2'><label className=''>30 de fevereiro</label></div>
+                    </div>
+                    <div className='parteMeio'>
+                    <div className='espaco1'><img className='icon' src="./imagens/relogio.png"/></div>
+                      <div className='espaco2'><label className=''>11:00</label></div>
+                    </div>
+                    <div className='parteBaixo'>
+                    <div className='espaco1'><img  className='icon' src="./imagens/gps.png"/></div>
+                    <div className='espaco2'><label className=''>Rod. joão gualberto suares - 999</label></div>
+                    </div>
+                    <div className='Divbotao'>
+                      <button className='botao'>alugar</button>
+                    </div>
+                  </div>
+              </div>
+              <div className='meioBaixo'>
+              <div className='quadrados4'>
+                    <div className='parteCima'>
+                      <div className='espaco1'><img className='icon' src="./imagens/calendario.png"/></div>
+                      <div className='espaco2'><label className=''>30 de fevereiro</label></div>
+                    </div>
+                    <div className='parteMeio'>
+                    <div className='espaco1'><img className='icon' src="./imagens/relogio.png"/></div>
+                      <div className='espaco2'><label className=''>21:00</label></div>
+                    </div>
+                    <div className='parteBaixo'>
+                    <div className='espaco1'><img  className='icon' src="./imagens/gps.png"/></div>
+                    <div className='espaco2'><label className=''>Rod. joão gualberto suares - 999</label></div>
+                    </div>
+                    <div className='Divbotao'>
+                      <button className='botao'>alugar</button>
+                    </div>
+                  </div>
+                  <div className='quadrados5'>
+                    <div className='parteCima'>
+                      <div className='espaco1'><img className='icon' src="./imagens/calendario.png"/></div>
+                      <div className='espaco2'><label className=''>30 de fevereiro</label></div>
+                    </div>
+                    <div className='parteMeio'>
+                    <div className='espaco1'><img className='icon' src="./imagens/relogio.png"/></div>
+                      <div className='espaco2'><label className=''>22:00</label></div>
+                    </div>
+                    <div className='parteBaixo'>
+                    <div className='espaco1'><img  className='icon' src="./imagens/gps.png"/></div>
+                    <div className='espaco2'><label className=''>Rod. joão gualberto suares - 999</label></div>
+                    </div>
+                    <div className='Divbotao'>
+                      <button className='botao'>alugar</button>
+                    </div>
+                  </div>
+                  <div className='quadrados6'>
+                    <div className='parteCima'>
+                      <div className='espaco1'><img className='icon' src="./imagens/calendario.png"/></div>
+                      <div className='espaco2'><label className=''>30 de fevereiro</label></div>
+                    </div>
+                    <div className='parteMeio'>
+                    <div className='espaco1'><img className='icon' src="./imagens/relogio.png"/></div>
+                      <div className='espaco2'><label className=''>23:00</label></div>
+                    </div>
+                    <div className='parteBaixo'>
+                    <div className='espaco1'><img  className='icon' src="./imagens/gps.png"/></div>
+                    <div className='espaco2'><label className=''>Rod. joão gualberto suares - 999</label></div>
+                    </div>
+                    <div className='Divbotao'>
+                      <button className='botao'>alugar</button>
+                    </div>
+                  </div>
+              </div>
+            </div>
+                  <div className='baixo'>
+                  </div>
+    </div>
+  );
+  
+};
+
+export default HorasPartida;
---
+++ tela_de_ListaHorarios/src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import HorasPartida from '../components/Body/HorasPartida.jsx'
+
+
+
+function Home() {
+  return (
+    <div>
+      
+       <HorasPartida/>
+     
+    </div>
+  )
+}
+
+export default Home
