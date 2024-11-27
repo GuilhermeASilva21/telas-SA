c05cd614c7b0838b6ba9c1fcbb7c6ce9512bb992
GUILHERME AGOSTINHO SILVA <guilherme_silva53@estudante.sesisenai.org.br>
2024-11-26 21:28:56 -0300

    tela da landing page

---
+++ tela_de_landing_page/src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import LandingPage from '../components/Body/LandingPage.jsx'
+
+
+
+function Home() {
+  return (
+    <div>
+      
+       <LandingPage/>
+     
+    </div>
+  )
+}
+
+export default Home
